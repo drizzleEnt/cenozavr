062ddb540780a9957a0326f98f5cecb94add1b3b
skitalex <drizzleent@yandex.ru>
2024-05-29 22:38:55 +0300

    add scraping logick for product

--- internal/service/scraper/service.go
+++ internal/service/scraper/service.go
@@ -2,8 +2,12 @@ package scraper
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"path"
+	"time"
 
+	"github.com/chromedp/cdproto/cdp"
 	"github.com/chromedp/cdproto/network"
 	"github.com/chromedp/chromedp"
 )
@@ -23,8 +27,11 @@ type Product struct {
 }
 
 const (
+	userAgent = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:126.0) Gecko/20100101 Firefox/126.0"
 	okeyMiaso = "https://www.okeydostavka.ru/spb/miaso-ptitsa-kolbasy/miaso-20"
-	okeyURL   = "https://www.okeydostavka.ru/"
+
+	okeyURL    = "https://www.okeydostavka.ru/"
+	okeyImgUrl = "https://36ltwco2hg.a.trbcdn.net"
 )
 
 func NewService() *srv {
@@ -32,44 +39,66 @@ func NewService() *srv {
 }
 
 func (s *srv) Scrap() error {
-
-	ctx, cancel := chromedp.NewContext(context.Background())
+	ctx, _ := chromedp.NewContext(context.Background())
+	ctx, cancel := context.WithTimeout(ctx, 10*time.Second)
 	defer cancel()
 
-	var good Product
-	err := chromedp.Run(ctx, tasks(&good))
-
-	if err != nil {
-		return err
+	if err := chromedp.Run(ctx, tasks()); err != nil {
+		return fmt.Errorf("could not navigate %s", err.Error())
 	}
-	log.Printf("Go's time.After example:\n%s", good.ProductName)
 
+	if err := chromedp.Run(ctx, chromedp.Sleep(2000*time.Millisecond) /*chromedp.WaitVisible("productListingWidget")*/); err != nil {
+		return fmt.Errorf("could not get section %s", err.Error())
+	}
+
+	var productsNode []*cdp.Node
+	if err := chromedp.Run(ctx, chromedp.Nodes(`.ok-theme.product`, &productsNode, chromedp.ByQueryAll)); err != nil {
+		return fmt.Errorf("could not get nodes %s", err.Error())
+	}
+
+	goods := make([]Product, 0, len(productsNode))
+
+	for _, v := range productsNode {
+		var product Product
+		//Get name
+		chromedp.Run(ctx, chromedp.Text(".product-name", &product.ProductName, chromedp.ByQuery, chromedp.FromNode(v)))
+
+		var ok bool
+
+		//Get product url
+		chromedp.Run(ctx, chromedp.AttributeValue(".product-name > a", "href", &product.ProductUrl, &ok, chromedp.ByQuery, chromedp.FromNode(v)))
+		if !ok {
+			log.Print("could not get product url")
+		}
+		product.ProductUrl = path.Join(okeyURL, product.ProductUrl)
+
+		//Get small image url
+		chromedp.Run(ctx, chromedp.AttributeValue(".product-image img", "data-src", &product.SmallImg, &ok, chromedp.ByQuery, chromedp.FromNode(v)))
+		if !ok {
+			log.Print("could not get small image url")
+		}
+		product.SmallImg = path.Join(okeyImgUrl, product.SmallImg)
+		product.BigImg = product.SmallImg
+
+		//Get old price
+		chromedp.Run(ctx, chromedp.Text(".label.crossed", &product.OldPrice, chromedp.ByQuery, chromedp.FromNode(v)))
+
+		//Get current price
+		chromedp.Run(ctx, chromedp.Text(".label.price:last-child", &product.Price, chromedp.ByQuery, chromedp.FromNode(v)))
+
+		goods = append(goods, product)
+	}
+
+	for _, v := range goods {
+		fmt.Println(v)
+	}
 	return nil
 }
 
-func tasks(g *Product) chromedp.Tasks {
+func tasks() chromedp.Tasks {
 	return chromedp.Tasks{
 		network.Enable(),
-		network.SetExtraHTTPHeaders(map[string]interface{}{"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:126.0) Gecko/20100101 Firefox/126.0"}),
+		network.SetExtraHTTPHeaders(map[string]interface{}{"User-Agent": userAgent}),
 		chromedp.Navigate(okeyMiaso),
-		chromedp.WaitVisible(`productListingWidget`),
-		chromedp.Text(`.product-name`, &g.ProductName, chromedp.ByQueryAll),
 	}
 }
-
-// func setHeaders(r *colly.Request) {
-// 	r.Headers.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8")
-// 	r.Headers.Set("Accept-Language", "en-GB,en;q=0.5")
-// 	r.Headers.Set("Accept-Encoding", "gzip, deflate, br, zstd")
-// 	r.Headers.Set("Connection", "keep-alive")
-// 	r.Headers.Set("Host", "www.okeydostavka.ru")
-
-// 	r.Headers.Set("DNT", "1")
-// 	r.Headers.Set("Sec-GPC", "1")
-// 	r.Headers.Set("Upgrade-Insecure-Requests", "1")
-// 	r.Headers.Set("Sec-Fetch-Dest", "document")
-// 	r.Headers.Set("Sec-Fetch-Mode", "navigate")
-// 	r.Headers.Set("Sec-Fetch-Site", "none")
-// 	r.Headers.Set("Sec-Fetch-User", "?1")
-// 	r.Headers.Set("Priority", "u=1")
-// }
